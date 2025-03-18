97773c7609c27321c311a8cb77bdb7e963a218ee
Berwyn-s <berwyn.surya007@gmail.com>
2025-03-18 22:06:46 +0700

    Fix base vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // for gh-pages deployment
+  base: '/porto-lisa-freelance/', // for github pages deployment. for dev, you can commment this line
   build: {
     outDir: 'dist',
   }
