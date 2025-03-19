3ca44ac137b85a81b05c0ea66a46b2b0c87b9cab
Ronald Sumichael <Ronald.Sumichael@gmail.com>
2025-03-19 22:13:49 +0700

    Fix: Update Color

--- src/pages/about_us/header_section/HeaderSection.jsx
+++ src/pages/about_us/header_section/HeaderSection.jsx
@@ -13,8 +13,8 @@ function HeaderSection() {
             {/* Banner */}
             <div className="bg-blue-800 text-center pt-5 pb-10 px-8 sm:pt-10 sm:pb-15 sm:px-12 md:pt-15 md:pb-20 md:px-23 lg:pt-20 lg:px-30 xl:pb-30 xl:px-40 relative">
                 <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-white font-bold w-4/5 text-left">
-                    Kenali kami untuk <span className="text-green-300">berijalan</span> mudah bagi
-                    bisnis mu
+                    Kenali kami untuk <span className="text-berijalan-green">berijalan</span> mudah
+                    bagi bisnis mu
                 </h1>
             </div>
 
